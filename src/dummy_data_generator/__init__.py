45d0d8048f8060e01cec93e7b7031804690ab723
bri-anadi <uix.brianadi@gmail.com>
2024-11-27 17:35:33 +0700

    init

---
+++ src/dummy_data_generator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Dummy Data Generator Library
+A comprehensive library for generating realistic dummy data for Indonesian context.
+"""
+
+from .generator import DummyDataGenerator
+
+__version__ = "0.1.0"
+__all__ = ['DummyDataGenerator']
